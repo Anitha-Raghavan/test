36ca504245614f0a691b46326f92a8deb647d24f
Anitha.R <anitha.r@target.com>
2020-02-29 17:03:46 +0530

    Added class number

--- src/com/tgt/igniteplus/Main.java
+++ src/com/tgt/igniteplus/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello ignite plus !!!");
+        System.out.println("Hello ignite plus 1 !!! ");
     }
 }
